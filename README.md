7359e6bdcd9805a573a2e515f5ea836063ff9e29
Kryštof Matějka <krystof.m@pm.me>
2020-07-08 22:38:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# next.js template
+
+Simple template for js web application
+
+## Contains
+
+ - next.js
+ - react
+ - typescript
+ - styled components
+
+## Quick start
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  color: #a71cff;
+`
+
+export const Home = () => (
+  <>
+    <Heading>Hello from next.js!</Heading>
+    <p>with styled components</p>
+  </>
+)
